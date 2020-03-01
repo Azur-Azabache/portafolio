640a4ee8e363c20e232a44f178259ad853f3b936
Azur-Azabache <tharuka_20@hotmail.com>
2020-03-01 18:21:01 -0500

    styles lightbox fresco

--- js/app.js
+++ js/app.js
@@ -16,4 +16,29 @@ $(document).ready(function($) {
 			} );
 		}
 	});
+
 });
+
+$("#watcher").click(function(){
+	$(".fr-content-element").addClass("zoom");
+	vacalola();
+});
+
+$('.zoom').hover(function vacalola() {
+	$(this).addClass('transition');
+}, function() {
+	$(this).removeClass('transition');
+});
+
+$('#mockup2').on('click', function(event) {
+    // the page will scroll up without this
+    event.preventDefault();
+
+    // Fresco API code goes here
+    Fresco.show([
+		{url: 'assets/images/home2.png', caption: 'Home - Intranet Universidad Continental' },
+		{ url: 'assets/images/lista_eventos.png', caption: 'Lista de Eventos' },
+		{ url: 'assets/images/recomendaciones.png', caption: 'Inicio y recomendaciones' },
+		{ url: 'assets/images/evento-detalles.png', caption: 'Detalles de los eventos' },
+	]);
+  });
